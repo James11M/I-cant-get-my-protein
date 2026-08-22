af1a563491dfcd6f10113fffe9555f9c09574459
James11M <james11marshall@icloud.com>
2026-08-22 13:31:36 +0100

    Restore product brand name

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -5,7 +5,7 @@ import { colours } from '@/theme/colours';
 export function Brand() {
   return (
     <View>
-      <Text style={styles.title}>I CAN'T HIT MY PROTEIN</Text>
+      <Text style={styles.title}>I CAN'T GET MY PROTEIN</Text>
       <Text style={styles.subtitle}>SCHOOL - SPORTS - AWARDS</Text>
     </View>
   );
